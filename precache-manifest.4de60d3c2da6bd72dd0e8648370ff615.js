e1ca63d3e84431fc55afadba40dd564101784c82
Sachin Gupta <sachinguptaa099@gmail.com>
2021-01-21 00:28:54 +0530

    Updates

---
+++ precache-manifest.4de60d3c2da6bd72dd0e8648370ff615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c4e51a686965ecd5965a92c18c3d08",
+    "url": "/alan-ai-news-app/index.html"
+  },
+  {
+    "revision": "a279eecc0dd6f60b9d6d",
+    "url": "/alan-ai-news-app/static/css/main.449f7370.chunk.css"
+  },
+  {
+    "revision": "667f7e8fec179846a53e",
+    "url": "/alan-ai-news-app/static/js/2.700c7c82.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/alan-ai-news-app/static/js/2.700c7c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a279eecc0dd6f60b9d6d",
+    "url": "/alan-ai-news-app/static/js/main.33011fe3.chunk.js"
+  },
+  {
+    "revision": "f03ca1a7ed6e86bdb53c",
+    "url": "/alan-ai-news-app/static/js/runtime-main.5d852fe0.js"
+  }
+]);
